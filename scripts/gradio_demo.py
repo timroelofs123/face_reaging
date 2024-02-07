7d8feacf56dadce32734778e089d592c483e1e55
Tim Roelofs <timroelofs123@gmail.com>
2024-02-07 23:53:12 +0100

    debugging

--- scripts/gradio_demo.py
+++ scripts/gradio_demo.py
@@ -88,7 +88,7 @@ def run(model_path):
                               title="Face Re-Aging Demo",
                               )
 
-    demo.launch()
+    demo.launch(debug=True)
 
 
 if __name__ == "__main__":
